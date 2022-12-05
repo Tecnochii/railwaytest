7916e310a84eeb7c038363dac3e57c878538471e
Emmanuel Leiva <ema62x@gmail.com>
2022-12-05 10:41:18 -0300

    Initial commit

---
+++ src/main/java/com/example/railwaytest/RailwaytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.railwaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwaytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwaytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/railwaytest/RailwaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.railwaytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwaytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
